6b63a887b8fe2d7f1d63c245f9e6661e3d8ee363
Laci <taddaddadada@ftddhdkeke.hu>
2025-03-15 18:46:54 -0400

    handleChange function added

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -9,6 +9,23 @@ const FeedbackForm = () => {
         feedback: ''
     });
 
+    //event as a pramater represent the input interaction
+    const handleChange = (event) => {
+
+    //This line extracts the name and value properties from the event object's target property. 
+    //The target property refers to the DOM element that triggered the event, which, in this case, is an input field. 
+    //The name property corresponds to the 'name' attribute of the input field, while the value property represents 
+    //the current 'value' entered by the user into the input field
+        const {name, value} = event.target;
+    //setFormData function is a state updater function provided by React's useState hook to update the state variable formData. 
+    //It spreads the existing formData object and then updates the property specified by the 'name' variable with the new value. 
+    //This pattern ensures that the state is updated immutably, meaning a new object is created with the updated property rather than modifying the existing state directly
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
   return (
     <>
     <nav>
